7995cfc1afa871a7ebfebbbc60358f40a393de75
gliburd218 <gregliburd628@gmail.com>
2024-11-19 17:20:48 +0000

    submission 4-6

--- .ccls-cache/@home@runner@chapter4-homework-and-labs-gliburd218/lab4-4.cpp
+++ .ccls-cache/@home@runner@chapter4-homework-and-labs-gliburd218/lab4-4.cpp
@@ -1,16 +1,23 @@
 //Lab4-4.cpp - displays the volume of a cylinder
 //Created/revised by <your name> on <current date>
+#include <iostream>
+#include <iomanip>
 
-{
-} //end of main function
-cin >> height;
-cout << "Height: ";
-cout << "Radius: ";
-cout << "Volume: " << volume << endl;
-double height   = 0.0;
-double radius   = 0.0;
-double volume   = 0.0;
-int main()
-return 0;
 using namespace std;
-volume = PI * radius * radius * height;
+
+const double PI = 3.14;
+
+int main() {
+  double height = 0.0;
+  cout << "Height: ";
+  cin >> height;
+
+  double radius = 0.0;
+  cout << "Radius: ";
+  cin >> radius;
+
+  double volume = PI * radius * radius * height;
+  cout << "Volume: " << volume << endl;
+
+  return 0;
+}
